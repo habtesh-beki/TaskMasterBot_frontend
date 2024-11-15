fdc830bbc24e052a6210099aceebf2661f3f02cc
Hatemariam <habteshbaki2014@gmail.com>
2024-11-15 12:14:58 +0300

    The token deleted

--- bot/bot.js
+++
@@ -1,9 +0,0 @@
-// import { Telegraf } from 'telegraf'
-
-// const Token = '7867102977:AAFoIfKEy0i9CrSF31xjqdS_ImDMfX8bo4Y'
-// const url_ngrok = 'https://36b3-197-156-73-167.ngrok-free.app/'
-// const bot = new Telegraf(Token)
-// bot.start((ctx) => ctx.reply('Hello', {
-//     reply_markup: {keyboard : [[{text: 'task' , web_app: {url:url_ngrok}}]]}
-// }))
-// bot.launch()
